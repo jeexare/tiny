d70e8870793b6532fa39106144d12221e3004e58
jeexare <heatherubiof@gmail.com>
2023-07-20 21:54:27 -0600

    bunch  of stuff today

--- js/barrasstats.js
+++ js/barrasstats.js
@@ -1,22 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Call the function to update the widths of 'bar-st' elements
-    updateStBarsWidth();
+  // Call the function to update the widths of 'bar-st' elements
+  updateStBarsWidth();
+});
+
+function updateStBarsWidth() {
+  // Get the elements with the class name 'bar-st'
+  const stBars = document.getElementsByClassName("bar-st");
+  if (stBars) {
+    // Iterate through each element
+    Array.from(stBars).forEach((stBar) => {
+      const max = parseInt(stBar.getAttribute("max"));
+      const act = parseInt(stBar.getAttribute("act"));
+      const name = stBar.getAttribute("name");
+
+      // Calculate the width as a percentage
+      const width = (act / max) * 100;
+
+      // Set the width of the ':after' pseudo-element
+      stBar.style.setProperty("--width", `${width}%`);
+
+      if (name) stBar.setAttribute("data-alt", `${act} / ${max} ${name}`);
     });
-          
-          function updateStBarsWidth() {
-      // Get the elements with the class name 'bar-st'
-      const stBars = document.getElementsByClassName("bar-st");
-      if (stBars) {
-        // Iterate through each element
-        Array.from(stBars).forEach((stBar) => {
-          const max = parseInt(stBar.getAttribute("max"));
-          const act = parseInt(stBar.getAttribute("act"));
-    
-          // Calculate the width as a percentage
-          const width = (act / max) * 100;
-    
-          // Set the width of the ':after' pseudo-element
-          stBar.style.setProperty("--width", `${width}%`);
-        });
-      }
-    }
+  }
+}
